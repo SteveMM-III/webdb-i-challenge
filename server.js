3789e8c756f177df871cc91aa1fb60d1163267b3
Kronykus <MoreheadSteve@gmail.com>
2019-12-09 15:33:24 -0600

    mvp

--- server.js
+++ server.js
@@ -1,9 +1,24 @@
 const express = require('express');
+const helmet = require( 'helmet' );
 
-const db = require('./data/dbConfig.js');
+const AccountsRouter = require( './accounts/accounts-router' );
 
 const server = express();
 
-server.use(express.json());
+function logger( req, res, next ) {
+  console.log( `${ req.method }to ${ req.originalUrl }` )
+  next();
+}
+
+server.get( '/', ( req, res ) => {
+  res.send( '<h3>DB Challenge I with knex</h3>' );
+} );
+
+
+server.use( helmet()       );
+server.use( express.json() );
+server.use( logger         );
+
+server.use( '/api/accounts', AccountsRouter );
 
 module.exports = server;
